c7bba6f3aafc1b99e502f3189effe1eaba533304
Semyon <Semyon.Fast@skoltech.ru>
2021-03-29 20:05:22 +0300

    commit

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -17,7 +17,7 @@ import hello.views
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
-    path('items/<str:newsSource>/',hello.views.render_items, name='source'),
+    path('items/<str:newsSourceName>/',hello.views.render_items, name='source'),
     path("AboutUs/",hello.views.AboutUs_page),
     path("admin/", admin.site.urls),
 ]
