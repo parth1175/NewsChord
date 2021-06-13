894a49965e559f717a76237ccaf2065b071c2f85
Semyon <Semyon.Fast@skoltech.ru>
2021-06-13 21:37:04 +0300

    new cards structure

--- hello/views.py
+++ hello/views.py
@@ -56,12 +56,6 @@ def render_items(request, newsSourceName):
 
     return render(request, 'items.html', {'newsSource': newsSourceName, 'articleCompounds':smallerArticleCompoundList})
 
-    # we want to render:
-        # Article names
-        # date published
-        # summary of article
-        # link to article
-
 def AboutUs_page(request):
     return render(request, 'AboutUs.html')
 
@@ -109,11 +103,9 @@ def index(request):
             print(query, flush=True)
             global enteredQuery # add store the query in the global variable
             enteredQuery = query
-            # menuSelect = request.POST.get('bias', False) #DropdownMenu.cleaned_data['bias'] #request.POST['bias']
             menuSelect = request.POST.get('searches', False)
             print("Form is valid", flush=True)
-            data = {'query': query}
-            form = TaskForm(data) # doing this allows you to present an empty form with the "render" statement
+            form = TaskForm({'query': query}) # doing this allows you to present an empty form with the "render" statement
             #https://docs.djangoproject.com/en/3.1/ref/forms/api/
 
 
@@ -123,60 +115,54 @@ def index(request):
         the NewsSources entries in the database
         """
         results = []
-        AllNewsSources = NewsSource.objects.all()
+        AllNewsSources = NewsSource.objects.filter(displayed=True) # filtered newsSources
+        AllIds = AllNewsSources.values_list('id',flat=True) # IDs of those filtered newsSources
         lowend,highend = 1,len(AllNewsSources) #start and end of the newsSource gathering
-        #highend = 8 #end of newsSource gathering
         bing_number_merge = 3 # there are 100 results max per request, 25-30 articles /month/newsSource
         newsSourcesData = []
-
-        for i in range(lowend, 4): # FOR DEVELOPMENT highend+1 create a list called newsSourcesData to gather desired newsSources
-            if (i != 9):# FOR THE GUARDIAN SKIP
-                newsSourcesData.append(AllNewsSources.get(pk=i))
-        responseSuccessful = False
-        resultsYeilded = False  # Global variables changed in GoogleURL() func
+        for i in AllIds[0:highend]: #FOR DEVELOPMENT highend create a list called newsSourceData to further filter and gather desired newsSources
+            newsSourcesData.append(AllNewsSources.get(pk=i))
+        responseSuccessful,resultsYeilded = False,False # Global variables changed in GoogleURL() func
         length_sources = len(newsSourcesData)
 
+
+
         """
-        add comments here....
+        This code segment adds to the 'results' list
+        'results' contains a list of a single article (for each NewsSource)
         """
         counter = 0
         while (counter < length_sources):
             print(f"Merged from {counter}, grouped by {bing_number_merge}", flush=True)
             size_merge = min(bing_number_merge, length_sources-counter)
             request_merged = bing_formrequest(query, size_merge, newsSourcesData, counter)
-            single_news_result = bing_newssearch(subscription_key_micro, request_merged, "Month", 100)#100 is MAX count for results list of single request
-            articles = [article for article in single_news_result['value']]
+            # articles = bing_newssearch(subscription_key_micro, request_merged, 100, "Month")#100 is MAX count for results list of single request
+            articles = bing_websearch(subscription_key_micro, request_merged, 100, "Month")
             print(f"There are {len(articles)} from {newsSourcesData[counter].homepage} ... {newsSourcesData[counter+size_merge-1].homepage}", flush=True)
             for k in range(size_merge):
                 if (len(articles) != 0):
                     article_chosen = bing_articlechoose(newsSourcesData[counter + k].homepage, articles, query)
                     #print(article_chosen["art"]['description'], flush = True)
                 else:
-                    article_chosen = {"art": "empty", "has_results": False}
-                #Perform extra search if there is no available data on the article
-                if (article_chosen["has_results"] == False):
+                # #Perform extra search if there is no available data on the article
                     source_request = query + " site:" + newsSourcesData[counter + k].homepage
-                    single_web_result = bing_websearch(subscription_key_micro, source_request, "Month", 40)
+                    web_results = bing_websearch(subscription_key_micro, source_request, 40, "Month")
                     print(f"Current source for extrasearch is {newsSourcesData[counter + k].homepage}", flush=True)
-                    if ('webPages' in single_web_result):
-                        web_results = [article for article in single_web_result['webPages']['value']]
+                    if(web_results == "empty"):
+                        article_chosen["art"] = "empty"
+                    else:
                         article_chosen["art"] = web_results[0]
                         print("Additional search helped")
-                    else:
-                        article_chosen["art"] = "empty"
-                        print("Empty article", flush=True)
                 results.append(article_chosen["art"])#chosen article from result list and added if it is relevant, else nothing
-                #print(article_chosen["art"], flush = True)
-                # if (article_chosen["art"] == "empty"):
-                #     print("Really empty article", flush=True)
             counter = counter + bing_number_merge
+
+
 
         """
         This code chunk creates a list for the:
         imageIndexes, article objects, links, leanings, reliabilties, and names
         for each element in the results[] list
         """
-        counter=0
         imageIndexes = []    # list of media source indexes to upload pictures and names
         articlesList = []    # list of article objects
         linksList = []       # list of links for each article
@@ -184,32 +170,15 @@ def index(request):
         reliabilityList = [] # list of the "reliabilty" of each news source
         sourceNameList = []  # list of the name of each news source
         colorList = []
-        for i in results:
-            if (counter == 8):#FOR THE GUARDIAN SKIP
-                counter += 1
+        for counter,i in enumerate(results):
             if i == "empty": #TODO: Process the empty one anyway and show user there are no results from this source
                 print(f"Counter in the if is {counter}", flush=True)
-                # leaning = adjust_leaning_wording(mediaOutlet.description)
-                # leaningList.append(leaning)
-                # if "Center" in leaning:
-                #     colorList.append("green")#0015ff87
-                # elif ("left" in leaning) or ("Left" in leaning):
-                #     colorList.append("blue")#ff000087
-                # elif ("right" in leaning) or ("Right" in leaning):
-                #     colorList.append("red")#00ff1587
-                # else: colorList.append("grey")#b5b5b5f1
-                # reliabilityList.append(mediaOutlet.cred)
-                # sourceNameList.append(mediaOutlet.newsSource)
-                # imageIndexes.append(counter+1)
-                # do nothing, just skip
                 print(f"The variable i in the if is {i}", flush=True)
-                counter += 1
             else:
                 # cannot pull from the newsSourcesData list because in django you cannot filter (.get()) once a slice has been taken. So using AllNewsSources instead
-                # Extra database request?
-                mediaOutlet = AllNewsSources.get(pk=lowend+counter) #the database object for the news source
+                # mediaOutlet = AllNewsSources.get(pk=lowend+counter) #the database object for the news source
+                mediaOutlet = AllNewsSources.get(pk=AllIds[counter])
                 if menuSelect == "all":
-                    print("It is all leanings >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",flush=True)
                     leaning = adjust_leaning_wording(mediaOutlet.description)
                     leaningList.append(leaning)
                     if "Center" in leaning:
@@ -221,54 +190,10 @@ def index(request):
                     else: colorList.append("grey")#b5b5b5f1
                     reliabilityList.append(mediaOutlet.cred)
                     sourceNameList.append(mediaOutlet.newsSource)
-                    imageIndexes.append(counter+1)
+                    imageIndexes.append(AllIds[counter])
                     #results is already article list of article objects
-                    print("Before fetching NewsSource", flush=True)
                     articlesList.append(i)  #article_processing(i["url"])) # IMPORTANT CHANGE: no manual article porcessing to increase performance
-                    print("After fetching", flush=True)
                     linksList.append(i["url"])
-                    # add all of them
-                elif(menuSelect == "left"):
-                    # only add the ones that equal left
-                    print("It is left >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",flush=True)
-                    leaning = adjust_leaning_wording(mediaOutlet.description)
-                    if("left" in leaning) or ("Left" in leaning):
-                        print("adding to the lists", flush=True)
-                        leaningList.append(leaning)
-                        colorList.append("blue")#ff000087
-                        reliabilityList.append(mediaOutlet.cred)
-                        sourceNameList.append(mediaOutlet.newsSource)
-                        imageIndexes.append(counter+1)
-                        articlesList.append(article_processing(i["url"]))
-                        linksList.append(i["url"])
-                    #else do nothing. discard
-                elif (menuSelect == "right") :
-                    # only add the ones that equal right
-                    print("It is right >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",flush=True)
-                    leaning = adjust_leaning_wording(mediaOutlet.description)
-                    if("right" in leaning) or ("Right" in leaning):
-                        leaningList.append(leaning)
-                        colorList.append("red")#ff000087
-                        reliabilityList.append(mediaOutlet.cred)
-                        sourceNameList.append(mediaOutlet.newsSource)
-                        imageIndexes.append(counter+1)
-                        articlesList.append(article_processing(i["url"]))
-                        linksList.append(i["url"])
-                    #else do nothing. discard
-                elif(menuSelect == "center"):
-                    # only add the ones that equal center
-                    leaning = adjust_leaning_wording(mediaOutlet.description)
-                    print("It is center >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",flush=True)
-                    if("Center" in leaning):
-                        leaningList.append(leaning)
-                        colorList.append("green")#ff000087
-                        reliabilityList.append(mediaOutlet.cred)
-                        sourceNameList.append(mediaOutlet.newsSource)
-                        imageIndexes.append(counter+1)
-                        articlesList.append(article_processing(i["url"]))
-                        linksList.append(i["url"])
-                    #else do nothing. discard
-                counter +=1
 
         """
         This code chunk obtains the summaries for each article in articlesList (previous chunk above)
@@ -278,7 +203,6 @@ def index(request):
         setOfRightArticleCompounds = []
         setOfLeftArticleCompounds = []
         setOfCenterArticleCompounds = []
-        setOfArticleCompounds = []
         index_of_article = 0 # used to index through the lists created above
         for article in articlesList:
             title = ""
@@ -291,8 +215,8 @@ def index(request):
                     date = datetime.date(int(date[0:4]), int(date[5:7]), int(date[8:10]))
                 else:
                     date = "Unknown publication date"
-                if ("description" in article):
-                    summary = article['description'] #''.join(sent+"." for sent in article_summary(article.text)) #IMPORTANT CHANGE: no manual article porcessing for this option
+                if ("snippet" in article):
+                    summary = article['snippet'] #''.join(sent+"." for sent in article_summary(article.text)) #IMPORTANT CHANGE: no manual article porcessing for this option
                 else:
                     summary = "Open the full article to know more"
                 title = clear_of_symbols(title)
@@ -311,10 +235,9 @@ def index(request):
                 setOfCenterArticleCompounds.append(a)
             elif (a.color == "red"):
                 setOfRightArticleCompounds.append(a)
-            setOfArticleCompounds.append(a)
             index_of_article += 1
 
-        return render(request, 'index.html', {'form': form, 'query':query, 'DropdownMenu':DropdownMenu, "articleCompounds": setOfArticleCompounds,
+        return render(request, 'index.html', {'form': form, 'query':query, 'DropdownMenu':DropdownMenu,
         "leftArticleCompounds": setOfLeftArticleCompounds, "centerArticleCompounds": setOfCenterArticleCompounds, "rightArticleCompounds": setOfRightArticleCompounds, "trending": trendingGoogle()})# re-renders the form with the url filled in and the url is passed to future html pages
         # setOfArticleCompounds is NOT utilized
     else:
